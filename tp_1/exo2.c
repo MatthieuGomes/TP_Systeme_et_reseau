a93289ea46a431cc09b27162d9dd3c94bcb2a61c
MatthieuGomes <141504473+MatthieuGomes@users.noreply.github.com>
2025-10-07 09:25:04 +0200

    moving files for new TP

---
+++ tp_1/exo2.c
@@ -0,0 +1,74 @@
+#include "basic_include.h"
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+char* file_path = "test.txt";
+
+int get_file_size(int file_descriptor){
+    struct stat  stat_struct;
+    if (fstat(file_descriptor, &stat_struct) == STD_FUNC_ERROR){
+        perror("fstat");
+        return STD_FUNC_ERROR;
+    }
+    return stat_struct.st_size;
+}
+
+int map_file_to_memory(char** mapped_file, int file_descriptor, int file_size){
+    *mapped_file = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
+    if (mapped_file == MAP_FAILED){
+        perror("mmap");
+        return STD_FUNC_ERROR;
+    }
+    return 0;
+}
+
+int invert_file_content(char** mapped_file, int file_size){
+    int byte_size = sizeof(char);
+    int half_file_size;
+    if(file_size%2*byte_size != 0){
+        half_file_size = (file_size/2)*byte_size + byte_size;
+    } else {
+        half_file_size = (file_size/2)*byte_size;
+    }
+    for (int i = 0; i < half_file_size; i+=byte_size){
+        char temp = (*mapped_file)[i];
+        (*mapped_file)[i] = (*mapped_file)[file_size - i - byte_size];
+        (*mapped_file)[file_size - i - byte_size] = temp;
+    }
+    return 0;
+}
+
+int main(){
+    
+    int file_descriptor = open(file_path, O_RDWR);
+    if (file_descriptor== STD_FUNC_ERROR){
+        perror("open");
+        close(file_descriptor);
+        return EXIT_FAILURE;
+    }
+    
+    int file_size = get_file_size(file_descriptor);
+    if (file_size == STD_FUNC_ERROR){
+        close(file_descriptor);
+        return EXIT_FAILURE;
+    }
+    printf("File size: %d bytes\n", file_size);
+    
+    char *mapped_file;
+    if (map_file_to_memory(&mapped_file, file_descriptor, file_size) == STD_FUNC_ERROR){
+        close(file_descriptor);
+        return EXIT_FAILURE;
+    }
+    
+    printf("Mapped file content:\n%s\n\n", mapped_file);
+
+    invert_file_content(&mapped_file, file_size);
+
+    printf("Inverted mapped file content:\n%s\n\n", mapped_file);
+    
+    munmap(mapped_file, file_size);
+    close(file_descriptor);
+    
+    return EXIT_SUCCESS;
+}
